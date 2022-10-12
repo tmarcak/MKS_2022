382b3f8a4152823b15238c257ddd58600726dfd5
Tomas Marcak <tmarcak@gmail.com>
2022-10-12 12:13:36 +0200

    Task 1

---
+++ cv03/Src/sct.h
@@ -0,0 +1,17 @@
+/*
+ * sct.h
+ *
+ *  Created on: 11. 10. 2022
+ *      Author:
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+#endif /* SCT_H_ */
